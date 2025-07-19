9b5912ac05d86b186cb71dbad3bc12bc8ceed333
Sadia Sakharkar <sakharkarsadia@gmail.com>
2025-07-19 13:45:07 +0530

    again commit

---
+++ LearnC++.cpp
@@ -0,0 +1,14 @@
+// cmd +shft + B to build
+#include<iostream>
+using namespace std;
+
+int main(){
+    // std:: cout << "Hello, CPP!" << std::endl;
+    // std:: cout << "Hello, CPP!" << "\n";
+
+    int x , y ; 
+    cin >> x >> y;
+    cout << "The value entered for X as " << x << " and the value for y as "<<y << endl;
+
+    return 0;
+}
