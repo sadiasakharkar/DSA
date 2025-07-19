6ac9214c4f275c015374f6c05b37b51beb70bb52
Sadia Sakharkar <sakharkarsadia@gmail.com>
2025-07-19 22:30:53 +0530

    ADT

---
+++ ADT_array.java
@@ -0,0 +1,8 @@
+public class ADT_array {
+    public static void main(String[] args) {
+        int[] arr = new int[5]; // CREATE()
+        arr[0] = 10; // STORE
+        arr[1] = 20;
+        System.out.println(arr[0]); // RETRIEVE
+    }
+}
