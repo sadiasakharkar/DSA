5b33fe8eab51c73b34c94a78b17e1cd604d72dda
Sadia Sakharkar <sakharkarsadia@gmail.com>
2025-08-22 00:39:25 +0530

    implemented binary search

---
+++ BinarySearch/BinarySearch.java
@@ -0,0 +1,52 @@
+package BinarySearch;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class BinarySearch {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the size of the array: ");
+        int size = sc.nextInt();
+
+        System.out.println("Enter the elements of the array: ");
+        int[] arr = new int[size];
+        for (int i = 0; i < size; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        System.out.println("Enter the target element:");
+        int target = sc.nextInt();
+
+        System.out.print("\nThe array you have entered: " + Arrays.toString(arr));
+
+        Arrays.sort(arr);
+        System.out.println("\nThe sorted array is: " + Arrays.toString(arr));
+
+        int ans = BinarySearch(arr, target);
+        if (ans == -1) {
+            System.out.println("Target element not found.");
+        } else {
+            System.out.println("The target element is found at index: " + ans);
+        }
+
+    }
+
+    static int BinarySearch(int[] arr, int target) {
+        int start = 0;
+        int end = arr.length - 1;
+
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+
+            if (target < arr[mid]) {
+                end = mid - 1;
+            } else if (target > arr[mid]) {
+                start = mid + 1;
+            } else {
+                return mid;
+            }
+        }
+        return -1;
+    }
+}
---
+++ BinarySearch/OrderAgnosticBS.java
@@ -0,0 +1,44 @@
+package BinarySearch;
+
+import java.util.Arrays;
+
+public class OrderAgnosticBS {
+    public static void main(String[] args) {
+        // int[] arr = { -6, -5, -2, 0, 1, 2, 3, 4, 5 };
+        int[] arr = { 5, 4, 3, 2, 1, 0, -1, -2, -3, -4, -5 };
+        int target = 0;
+        int ans = OrderAgnosticBS(arr, target);
+        System.out.println(ans);
+    }
+
+    static int OrderAgnosticBS(int[] arr, int target) {
+        int start = 0;
+        int end = arr.length - 1;
+
+        // to find if teh expression is in asc or desc order
+        boolean isAsc = arr[start] < arr[end];
+
+        while (start <= end) {
+
+            int mid = start + (end - start) / 2;
+
+            if (arr[mid] == target) {
+                return mid;
+            }
+            if (isAsc) {
+                if (target < arr[mid]) {
+                    end = mid - 1;
+                } else if (target > arr[mid]) {
+                    start = mid + 1;
+                }
+            } else {
+                if (target < arr[mid]) {
+                    start = mid + 1;
+                } else if (target > arr[mid]) {
+                    end = mid - 1;
+                }
+            }
+        }
+        return -1;
+    }
+}
