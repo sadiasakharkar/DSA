0f2e787306e670b87983f555aae92d7650f1225f
Sadia Sakharkar <sakharkarsadia@gmail.com>
2025-08-22 23:06:35 +0530

    Bubble Sort

--- BubbleSort/BubbleSort.java
+++ BubbleSort/BubbleSort.java
@@ -23,6 +23,7 @@ public class BubbleSort {
                     int temp = arr[j];
                     arr[j] = arr[j - 1];
                     arr[j - 1] = temp;
+
                     swapped = true;
                 }
             }
